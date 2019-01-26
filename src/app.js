5b128f35b672d62accf9b7207ded6609bf403f42
WuMat <wuzikm@gmail.com>
2019-01-26 20:40:25 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+
+const app = express()
+
+console.log('dziala')
+
+
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "@babel/polyfill";
+import http from 'http';
+import app from './app';
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+
+server.listen(port)
+console.log('app run')
