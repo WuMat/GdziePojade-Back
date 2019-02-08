ee6c3bbbdfc23015d1fc5ea22a1e512b4366ee60
WuMat <wuzikm@gmail.com>
2019-02-08 12:19:20 +0100

    connecting with mLab, add nodemon to automatic restart app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
+dotenv.config();
 
-const app = express()
+const DBNAME = process.env.DB_NAME;
+const DBPASSWORD = process.env.DB_PASSWORD;
 
-console.log('dziala')
+mongoose
+  .connect(
+    `mongodb://${DBNAME}:${DBPASSWORD}@ds125945.mlab.com:25945/gdziepojadepl`,
+    { useNewUrlParser: true }
+  )
+  .then(result => console.log("database is connected"))
+  .catch(err => console.log(err));
 
+const app = express();
 
+console.log("dziala");
 
 export default app;
